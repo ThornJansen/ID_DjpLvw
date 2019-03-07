74ee2c007ebea1a362fdfccfba57d48c8612d5d7
ThornJansen <thorn.prive@gmail.com>
2019-03-07 21:10:18 +0100

    New views

--- q2Create.sql
+++ q2Create.sql
@@ -1,3 +1,29 @@
-CREATE MATERIALIZED VIEW grade5 AS SELECT * FROM CourseRegistrations WHERE Grade >= 5;
+CREATE MATERIALIZED VIEW grade5 AS (
+	SELECT * FROM CourseRegistrations WHERE Grade >= 5
+);
+
 CREATE INDEX grade5_sr_idx ON grade5 (StudentRegistrationId);
-CREATE MATERIALIZED VIEW Active AS (SELECT srd.StudentId, srd.DegreeId, srd.StudentRegistrationId, SUM(cr.Grade*Courses.ECTS)/SUM(Courses.ECTS) AS gpa FROM StudentRegistrationsToDegrees as srd INNER JOIN Degrees ON srd.DegreeId = Degrees.DegreeId INNER JOIN CourseRegistrations AS cr ON srd.StudentRegistrationId = cr.StudentRegistrationId INNER JOIN CourseOffers as co ON cr.CourseOfferId = co.CourseOfferId INNER JOIN Courses ON co.CourseId = Courses.CourseId WHERE cr.Grade >= 5 GROUP BY srd.StudentId, srd.DegreeId, Degrees.TotalECTS, srd.StudentRegistrationId HAVING SUM(Courses.ECTS) < Degrees.TotalECTS OR SUM(Courses.ECTS) IS NULL);
+
+CREATE MATERIALIZED VIEW AllStudentsDegrees AS (
+	SELECT srd.StudentId, srd.DegreeId, srd.StudentRegistrationId, Degrees.TotalECTS
+	FROM StudentRegistrationsToDegrees as srd
+	INNER JOIN Degrees ON srd.DegreeId = Degrees.DegreeId
+);
+
+CREATE MATERIALIZED VIEW CompletedDegrees2 AS (
+	SELECT asd.StudentId, asd.DegreeId, asd.StudentRegistrationId, asd.TotalECTS
+	FROM AllStudentsDegrees AS asd
+	INNER JOIN grade5 AS cr ON asd.StudentRegistrationId = cr.StudentRegistrationId
+	INNER JOIN CourseOffers as co ON cr.CourseOfferId = co.CourseOfferId
+	INNER JOIN Courses ON co.CourseId = Courses.CourseId	
+	GROUP BY asd.StudentId, asd.DegreeId, asd.TotalECTS, asd.StudentRegistrationId
+	HAVING SUM(Courses.ECTS) < asd.TotalECTS
+);
+
+CREATE MATERIALIZED VIEW ActiveDegrees AS (	
+	SELECT asd.StudentId, asd.DegreeId, asd.StudentRegistrationId
+	FROM AllStudentsDegrees AS asd
+	EXCEPT
+	SELECT cd.StudentId, cd.DegreeId, cd.StudentRegistrationId
+	FROM CompletedDegrees AS cd
+);
